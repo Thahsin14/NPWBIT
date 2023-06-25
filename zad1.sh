d7fd957268b3fbcd0bfc8ac052dfeeb7782c4529
unknown <s.puczka@gmail.com>
2023-06-25 21:47:11 +0200

    logs

--- zad1.sh
+++ zad1.sh
@@ -1,4 +1,18 @@
-if [ "$1" = "--date" ]; then
+if [ "$1" = "--logs" ]; then
+    if [ -n "$2" ]; then
+        for ((i=1; i<=$2; i++)); do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Skrypt: skrypt.sh" >> log$i.txt
+            echo "Data: $(date)" >> log$i.txt
+        done
+    else
+        for ((i=1; i<=100; i++)); do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Skrypt: skrypt.sh" >> log$i.txt
+            echo "Data: $(date)" >> log$i.txt
+        done
+    fi
+elif [ "$1" = "--date" ]; then
     echo "Dzisiejsza data: $(date)"
 else
     echo "Nieznane. Użyj --help"
