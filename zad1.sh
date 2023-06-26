a4d819e61e27c3d34b4ebc7bae3772b0b8cc9c54
unknown <s.puczka@gmail.com>
2023-06-26 23:38:37 +0200

    cz2

--- zad1.sh
+++ zad1.sh
@@ -1,23 +1,44 @@
-if [ "$1" = "--logs" ]; then
+if [ "$1" = "--logs" ]|[ "$1" = "-l" ]; then
     if [ -n "$2" ]; then
         for ((i=1; i<=$2; i++)); do
-            echo "Nazwa pliku: log$i.txt" > log$i.txt
-            echo "Skrypt: zad1.sh" >> log$i.txt
-            echo "Data: $(date)" >> log$i.txt
+            mkdir log$i
+            echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
+            echo "Skrypt: zad1.sh" >> log$i/log$i.txt
+            echo "Data: $(date)" >> log$i/log$i.txt
         done
     else
         for ((i=1; i<=100; i++)); do
-            echo "Nazwa pliku: log$i.txt" > log$i.txt
-            echo "Skrypt: zad1.sh" >> log$i.txt
-            echo "Data: $(date)" >> log$i.txt
+            mkdir log$i
+            echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
+            echo "Skrypt: zad1.sh" >> log$i/log$i.txt
+            echo "Data: $(date)" >> log$i/log$i.txt
         done
     fi
-elif [ "$1" = "--help" ]; then
-    echo "    --help - wyswietlenie opcji."
-    echo "    --logs - utworzenie plików log"
-    echo "    --logs [ilosc] utworzenie danej ilosci logow"
-    echo "    --date - wyświetlenie dzisiejszej daty"
-elif [ "$1" = "--date" ]; then
+elif [ "$1" = "--error"]|[ "$1" = "-e" ]; then
+    if [ -n "$2" ]; then
+        for ((i=1; i<=$2; i++)); do
+            mkdir error$i
+            echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+            echo "Skrypt: zad1.sh" >> error$i/error$1.txt
+            echo "Data: $(date)" >> error$i/error$1.txt
+        done
+    else
+        for ((i=1; i<=100; i++)); do
+            mkdir error$i
+            echo "Nazwa pliku: error$i.txt" > error$i/error$1.txt
+            echo "Skrypt: zad1.sh" >> error$i/error$1.txt
+            echo "Data: $(date)" >> error$i/error$1.txt
+        done
+    fi
+elif [ "$1" = "--init" ]; then
+    git clone https://github.com/Thahsin14/NPWBIT
+
+elif [ "$1" = "--help" ]|[ "$1" = "-h" ]; then
+    echo "    -h --help - wyswietlenie opcji."
+    echo "    -l --logs - utworzenie 100 plików log"
+    echo "    -l --logs [ilosc] utworzenie danej ilosci logow"
+    echo "    -d --date - wyświetlenie dzisiejszej daty"
+elif [ "$1" = "--date" ]|[ "$1" = "-d" ]; then
     echo "Dzisiejsza data: $(date)"
 else
     echo "Nieznane. Użyj --help"
