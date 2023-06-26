2b46a3bbe0a486d9b61482c440d7d138af1597f7
Thahsin14 <118362996+Thahsin14@users.noreply.github.com>
2023-06-26 22:28:57 +0200

    Update zad1.sh

--- zad1.sh
+++ zad1.sh
@@ -2,13 +2,13 @@ if [ "$1" = "--logs" ]; then
     if [ -n "$2" ]; then
         for ((i=1; i<=$2; i++)); do
             echo "Nazwa pliku: log$i.txt" > log$i.txt
-            echo "Skrypt: skrypt.sh" >> log$i.txt
+            echo "Skrypt: zad1.sh" >> log$i.txt
             echo "Data: $(date)" >> log$i.txt
         done
     else
         for ((i=1; i<=100; i++)); do
             echo "Nazwa pliku: log$i.txt" > log$i.txt
-            echo "Skrypt: skrypt.sh" >> log$i.txt
+            echo "Skrypt: zad1.sh" >> log$i.txt
             echo "Data: $(date)" >> log$i.txt
         done
     fi
